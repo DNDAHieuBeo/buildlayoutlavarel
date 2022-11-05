71bc35ec0b41c9f11d313eadccddd00a5b21feba
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2022-11-05 07:26:57 +0700

    test

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Slide;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class PageController extends Controller
+{
+
+    public function getIndex()
+    {
+        $slide = Slide::all();
+        $new_product = Product::where('new', 1)->get();
+        return view('page.trangchu', compact('slide','new_product'));
+    }
+
+    public function getLoaiSp()
+    {
+        return view('page.loai_sanpham');
+    }
+
+    public function getChitiet()
+    {
+        return view('page.chitiet_sanpham');
+    }
+
+    public function getLienhe()
+    {
+        return view('page.lienhe');
+    }
+
+    public function getAbout()
+    {
+        return view('page.about');
+    }
+}
